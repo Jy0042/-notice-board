a13bdb0276fd5d38111b643754e56606bd5333d6
Jy0042 <sjy6630@gmail.com>
2023-03-16 00:58:30 +0900

    React JS #6 이벤트 처리

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,3 +1,32 @@
 export default function Hello() {
-    return <h1>Hello</h1>;
+
+    function showName() {
+        console.log('Mike');
+    }
+    function showAge(age) {
+        console.log(age);
+    }
+    function showText(txt) {
+        console.log(txt);
+    }
+
+    return (
+        <div>
+            <h1>Hello</h1>
+            <button onClick={showName}>Show name</button>
+            <button onClick={() => {
+                    showAge(10)
+                }}
+            >
+                Show age
+            </button>
+            <input 
+                type="text" 
+                onChange={e => {
+                    const txt = e.target.value;
+                    showText(txt);
+                }}
+            />
+        </div>
+    );
 }
