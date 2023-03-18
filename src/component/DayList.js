df04fafcf70b01e2f8610f8a03beaff6c08c900d
Jy0042 <sjy6630@gmail.com>
2023-03-19 04:45:07 +0900

    느린 인터넷 환경에서 구동 시 로딩중 문구 나오게 추가, 저장 단추 한번만 동작할수있게 변경

--- src/component/DayList.js
+++ src/component/DayList.js
@@ -5,6 +5,10 @@ import uesFetch from "./hooks/useFetch";
 export default function DayList() {
     const days = uesFetch("http://localhost:3001/days")
     
+    if(days.length === 0){
+        return <span>Loading... 기둘 ㄱㄷㄱㄷ</span>
+    }
+
     return (
         <ul className="list_day">
             {days.map(day => (
