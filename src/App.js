ee1b275ff20f0b4f3b7eb085e6d0a44c0f45aef6
Jy0042 <sjy6630@gmail.com>
2023-03-19 04:28:27 +0900

    POST(생성)을 활용하여 단어, day 추가 버튼 구현, usehistory 사용하여 생성 된 페이지로 이동 구현 (최신 버전에 맞게 적용)'

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import DayList from "./component/DayList";
 import Header from "./component/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import EmptyPage from "./component/EmptyPage";
+import CreateWord from "./component/CreateWord";
+import CreateDay from "./component/CreateDay";
 
 function App() {
   return ( 
@@ -13,6 +15,8 @@ function App() {
             <Route path="/" element={<DayList />} />
             <Route path="/day/:day" element={<Day />} />
             <Route path="*" element={<EmptyPage />} />
+            <Route path="/create_word" element={<CreateWord />} />
+            <Route path="/create_day" element={<CreateDay />} />
           </Routes>
         </BrowserRouter>
       </div>
